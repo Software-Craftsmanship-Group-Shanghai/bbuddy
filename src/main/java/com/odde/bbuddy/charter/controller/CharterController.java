404693e26186bc4d61b940bf96a8aebccab5f490
张锦华 <zhang_colin@163.com>
2017-02-19 13:25:59 +0800

    Add fake calculator to charter controller

--- src/main/java/com/odde/bbuddy/charter/controller/CharterController.java
+++ src/main/java/com/odde/bbuddy/charter/controller/CharterController.java
@@ -1,5 +1,6 @@
 package com.odde.bbuddy.charter.controller;
 
+import com.odde.bbuddy.charter.domain.CharterCalculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/charter")
 public class CharterController {
+    private CharterCalculator calculator;
+
+    @Autowired
+    public CharterController(CharterCalculator calculator) {
+        this.calculator = calculator;
+    }
 
     @GetMapping("/fee_result")
     public String feeResult(Model model) {
-        model.addAttribute("result", 560);
+        model.addAttribute("result", calculator.calculate());
         return "/charter/fee_result";
     }
 }
--- src/test/java/com/odde/bbuddy/charter/controller/CharterControllerTest.java
+++ src/test/java/com/odde/bbuddy/charter/controller/CharterControllerTest.java
@@ -1,17 +1,22 @@
 package com.odde.bbuddy.charter.controller;
 
+import com.odde.bbuddy.charter.domain.CharterCalculator;
 import org.junit.Test;
 import org.springframework.ui.Model;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class CharterControllerTest {
     @Test
     public void get_result() {
-        CharterController controller = new CharterController();
+        CharterCalculator calculator = mock(CharterCalculator.class);
+        CharterController controller = new CharterController(calculator);
+        int feeResult = 530;
+        when(calculator.calculate()).thenReturn(feeResult);
         Model model = mock(Model.class);
         controller.feeResult(model);
-        verify(model).addAttribute("result", 560);
+        verify(model).addAttribute("result", feeResult);
     }
 }
